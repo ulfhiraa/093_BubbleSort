e44e8f6acc329b09233fb8374166c7b2bbaa70d5
ulfhiraa <studyiraa@gmail.com>
2023-03-17 16:38:31 +0700

    prosedur untuk mengurutkan array

--- bubblesort/bubblesort.cpp
+++ bubblesort/bubblesort.cpp
@@ -46,3 +46,22 @@ void unsorted() {
     }
     cout << endl;
 }
+
+void display() {
+    cout << endl;    // procedur untuk menampilkan hasil
+    cout << "================================= " << endl;    // Output ke layar
+    cout << "Element Array yang telah tersusun" << endl;        // Output ke layar
+    cout << "==================================" << endl;    // Output ke layar
+    for (int j = 0; j < n; j++) {            // Looping dengan j dimulai dari 0 hingga n-1
+        cout << a[j] << endl;                // Output ke layar
+    }
+    cout << endl;                            // Output baris kosong
+}
+
+int main() {
+    input()       // Memanggil procedur input()
+    unsorted();      //Memanggil procedur unsorted() dari
+    bubbleSortArray();      // Memanggil procedur bubbleSortArray()
+    display();      // Memanggil procedur display()
+    return 0;
+}
