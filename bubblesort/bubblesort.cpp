0d4cc3a6363ec4b78662d57ea6a0dced785aae4d
ulfhiraa <studyiraa@gmail.com>
2023-03-17 16:07:47 +0700

    MENAMBAHKAN variable a dengan ukuran 20 dan variable n

--- bubblesort/bubblesort.cpp
+++ bubblesort/bubblesort.cpp
@@ -1,20 +1,28 @@
- // bubblesort.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int a[20];           // Deklarasi array a dengan ukuran 20
+int n;               // Deklarasi variabel n  untuk menyimpan banyaknya elemen pada array
+
+void input() {       // procedur untuk input
+    while (true) {     // Looping
+        cout << "Masukan banyaknya elemen pada array : ";  //Output ke layar
+        cin >> n;    // Input dari pengguna
+        if (n <= 20) // Jika n kurang dari atau sama dengan 20
+            break;   // Keluar dari loop
+        else {       // Jika n lebih dari 20
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"; // Output ke layar
+        }
+    }
+    cout << endl;                            // Ouput baris kosong
+    cout << "====================" << endl;  // Output ke layar
+    cout << "Masukkan Elemen Array" << endl; // Output ke layar
+    cout << "====================" << endl;  // Output ke layar
+
+    for (int i = 0; i < n; i++) {     // Looping dengan i dimulai dari 0 hingga n-1
+        cout << "Data ke-" << (i + 1) << ": ";     // Output ke layar
+        cin >> a[i];                     // Input dari pengguna
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
